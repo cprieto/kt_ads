b8dd3115f2ed8cf6b3645cd8a5a8b2b1419450bb
Cristian Prieto <me@cprieto.com>
2020-09-07 14:15:21 +0200

    Hash tables
     - Add extend and rehash

--- src/main/kotlin/hash.kt
+++ src/main/kotlin/hash.kt
@@ -1,6 +1,6 @@
 abstract class Hash() {
-    protected open var buckets: Array<Int?> = arrayOfNulls(6)
-    protected open fun hash(value: Int): Int = value % (size + 1)
+    protected open var buckets: Array<Int?> = arrayOfNulls(7)
+    protected open fun hash(value: Int): Int = value % buckets.size
 
     abstract fun add(value: Int)
 
@@ -18,7 +18,7 @@ class HashCollisionException(message: String): Exception(message)
 /**
  * A hash table that once is a collision it throws an exception
  */
-class CollisionHash(size: Int = 6): Hash() {
+class CollisionHash(size: Int = 7): Hash() {
     override fun add(value: Int) {
         val key = hash(value)
         if (buckets[key] != null && buckets[key] != value)
@@ -35,16 +35,20 @@ class CollisionHash(size: Int = 6): Hash() {
 class ExtendHash(val growRate: Double = 1.5): Hash() {
     private fun rehash() {
         val newSize = (size * growRate).toInt()
-        val resized = arrayOfNulls<Int?>(newSize)
+        val items = buckets.copyOf()
 
-        for (elem in buckets.filterNotNull())
+        buckets = arrayOfNulls(newSize)
+        for (elem in items.filterNotNull())
             add(elem)
-
-        buckets = resized
     }
 
     override fun add(value: Int) {
-        TODO("Not yet implemented")
+        val key = hash(value)
+        if (buckets[key] != null && buckets[key] != value) {
+            rehash()
+            add(value)
+        }
+        else buckets[key] = value
     }
 }
 
@@ -56,6 +60,19 @@ class ExtendHash(val growRate: Double = 1.5): Hash() {
 class LinearProbingHash(initialSize: Int): Hash() {
     override var buckets: Array<Int?> = arrayOfNulls(initialSize)
     override fun add(value: Int) {
-        TODO("Not yet implemented")
+        var key = hash(value)
+        if (buckets[key] != null && buckets[key] == value)
+            linearProbing(key, value)
+        else buckets[key] = value
+    }
+
+    private fun linearProbing(key: Int, value: Int) {
+        for ((idx, content) in buckets.withIndex()) {
+            when {
+                content == value -> break
+                content == null -> buckets[idx] = value
+            }
+        }
+
     }
 }
